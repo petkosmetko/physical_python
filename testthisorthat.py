e35c7fbc7329390da5ec42f281daf1ede82311bb
Peter Gruber <pgruber1403@gmail.com>
2025-08-20 18:28:53 +0200

    Add files via upload

---
+++ testthisorthat.py
@@ -0,0 +1,19 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("This or that")
+        self.geometry(f"{1440/3}x{3200/3}")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
